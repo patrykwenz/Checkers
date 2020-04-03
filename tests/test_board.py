744a76ea177393d1a0d476c6d8b5c9356d293d44
patrykwenz <pw7f430@gmail.com>
2020-04-03 18:05:15 +0200

    add requirements.txt

--- tests/test_board.py
+++ tests/test_board.py
@@ -2,8 +2,4 @@ from unittest import TestCase
 from src.board import Board
 
 class TestBoard(TestCase):
-    def test_get_board(self):
-        t = Board()
-        b = t.get_board()
-        for i in b:
-            print(i)
+    pass
