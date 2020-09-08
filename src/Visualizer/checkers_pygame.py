67f4890966cc88fb81b39006048bb282dccdb88b
Marcin Borysiewicz <frugoxmaniak@gmail.com>
2020-09-08 21:25:03 +0000

    Threading visualizer

--- src/Visualizer/checkers_pygame.py
+++ src/Visualizer/checkers_pygame.py
@@ -158,3 +158,4 @@ def start_visualizer():
         pygame.display.flip()
 
     pygame.quit()
+
--- src/game_exe.py
+++ src/game_exe.py
@@ -3,13 +3,17 @@ from src.RulesModule import rules
 from src.VisionModule import image_processing
 from src.RulesModule.board import Board
 
+from threading import Thread
+
 if __name__ == '__main__':
     name = "plansza0x.png"
     previous_board = Board(name.replace("x", str(0)))
     next_board = Board(name.replace("x", str(1)))
     queue = [rules.try_to_get_move_category(previous_board, next_board)]
     i = 2
-    checkers_pygame.start_visualizer()
+
+    visual_thread = Thread(target=checkers_pygame.start_visualizer())
+    visual_thread.start()
 
     while True:
         if len(queue) > 0:
